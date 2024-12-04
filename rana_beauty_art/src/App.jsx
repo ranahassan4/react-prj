0ff81861027180c88452fb4bb061e806776fa2bd
ranahassan4 <ranahassanb204@gmail.com>
2024-12-04 16:31:42 +0200

    ..

--- rana_beauty_art/src/App.jsx
+++ rana_beauty_art/src/App.jsx
@@ -9,7 +9,7 @@ import DeliveryList from "./components/DeliveryList";
 import Cart from "./components/Cart";
 import ProdCreate from "./Dashboardpages/ProdCreate"; // AA
 import Dashboard from "./Dashboardpages/Dashboard"; // AA
-cd;
+
 const App = () => {
   return (
     <BrowserRouter>
