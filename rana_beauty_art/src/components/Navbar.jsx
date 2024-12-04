85d378a63454d86b41ec5683b5a3c73404de8cd0
ranahassan4 <ranahassanb204@gmail.com>
2024-12-04 19:40:16 +0200

    .

--- rana_beauty_art/src/components/Navbar.jsx
+++ rana_beauty_art/src/components/Navbar.jsx
@@ -1,40 +1,53 @@
-import "../CSS/Navbar.css";
+// import "../CSS/Navbar.css";
+
 import { Link } from "react-router-dom";
+import "../CSS/navBar.css";
 import logo from "../assets/ahmad/logo.png";
 import user from "../assets/ahmad/User.svg";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="Logo" />
-        </Link>
+    <div>
+      {/* Announcement Bar */}
+      <div className="announcement-bar">
+        <p>
+          🎄 <strong>Its Christmas!</strong> During December, enjoy{" "}
+          <strong>33% off</strong> on Solitaire Rings and Necklaces! 🎁
+        </p>
       </div>
 
-      <div className="nav-links">
-        <Link to="/shop">Shop</Link>
-        <Link to="/women">New</Link>
-        <Link to="/kids">Trend</Link>
-        <Link to="/sale">Sale</Link>
-      </div>
+      {/* Navigation Bar */}
+      <nav className="navbar">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+        </div>
 
-      <div className="nav-actions">
-        <Link to="/Signin_up">
-          <img src={user} alt="user logo" />
-        </Link>
+        <div className="nav-links">
+          <Link to="/shop">Shop</Link>
+          <Link to="/women">New</Link>
+          <Link to="/kids">Trend</Link>
+          <Link to="/sale">Sale</Link>
+        </div>
 
-        <input
-          type="text"
-          className="search-bar"
-          placeholder="Search for items..."
-        />
+        <div className="nav-actions">
+          <Link to="/Signin_up">
+            <img src={user} alt="user logo" />
+          </Link>
 
-        <Link to="/Cart" className="cart-button">
-          Cart 🛒
-        </Link>
-      </div>
-    </nav>
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search for items..."
+          />
+
+          <Link to="/Cart" className="cart-button">
+            Cart 🛒
+          </Link>
+        </div>
+      </nav>
+    </div>
   );
 };
 
